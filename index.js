36f8f06666881e507248bffddca3a38c630a5cda
Katy Carr <khcarr9@gmail.com>
2017-10-20 20:22:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function addToCart(item) {
 function viewCart() {
   if(cart.length===0) {
     console.log("Your shopping cart is empty.")
-  }  else {
-    var string = "In your cart, you have ";
-    if(cart.length===1) {
+  } else if(cart.length===1) {
+      var string = "In your cart, you have ";
       var itemObj = cart[0];
       var itemName = Object.keys(itemObj);
       var itemPrice = itemObj[itemName];
@@ -42,7 +41,7 @@ function viewCart() {
 
 
   }
-}
+
 /*
 function total() {
   // write your code here
